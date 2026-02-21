436309c85bdd3c75410b03e92ab7872cc6fff667
Nguoi iu Ngoc Ha <tuananh020400@gmail.com>
2026-02-21 22:39:53 +0900

    add loginview and model candidate, company

--- skillrank/apps/core/models.py
+++ skillrank/apps/core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Candidate(models.Model):
+    name = models.CharField(max_length=255)
+    university = models.CharField(max_length=255)
+    major = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    taxid = models.CharField(max_length=255, unique=True)
+    major = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
